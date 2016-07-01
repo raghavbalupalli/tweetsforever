5b3005a700edefccf72e15cda70fd326f43c0357
raghavbalupalli <raghavbalupalli@gmail.com>
2016-07-01 15:12:48 -0700

    security added to devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
 
-  get 'pages/index'
-
-  get 'pages/home'
-
-  get 'pages/profile'
-
-  get 'pages/explore'
  
-  resources :pages
+
+  get '/home' => 'pages#home'
+
+  get  '/user/:id' => 'pages#profile'
+
+  get '/explore' => 'pages#explore'
+ 
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
